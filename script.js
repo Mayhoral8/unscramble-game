2a14c2a70d01d62190908e0299b72a38be67f12f
Mayowa Ayeni <Mayhoral008@gmail.com>
2022-07-22 13:06:29 +0100

    f

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 'use-strict'
 
 
@@ -7,7 +8,7 @@ let sportRaw = 'athletics spectator sport competition game racing gymnastics spo
 let socialRaw =  'favourite downvote twitter facebook network linkedin google viral marketing google event website reach telegraph Follower Like Share Tweet Comment Viral Trend DM Feed Mention Reply Traffic Thread Filter TikTok Snapchat WhatsApp Instagram Retweet Block Meme Skit YouTube  Post Upload Content Video Picture Space Group chat Forum Live Stream Bookmark Profile Status Discord Emoji Fan Fleet Friend Handle Influencer Mention Mute Pin Snap Sticker Selfie WeChat Vlog Verified hashtag shareable social trend media post unfollow';
 const sportFilter = sportRaw.split(' ').filter(currWord => (currWord != '')).map(currWord => (currWord.replace(currWord[0], currWord[0].toLocaleUpperCase())))
 const sport = sportFilter;
-console.log(sport)
+// console.log(sport)
 const social = socialRaw.split(' ');
 const arrCat = {
     Medical: ['Blood', 'Hospital', 'Medicine', 'Theatre', 'Admire'],
@@ -33,14 +34,14 @@ const btn = document.querySelector('.btn')
 let noAttempts = document.querySelector('.attemptsV');
 let highscore = document.querySelector('.HighScore');
 const newWord = document.querySelector('.newWord');
-
-header.classList.contains('bt ')
+const tilesCon = document.querySelector('.tiles')
+// header.classList.contains('bt ')
 catsocialMedia.addEventListener('click', function(){
     mode = true;
     arrWords = arrCat.social;
-    document.querySelector('.word').classList.remove('btnWrong')
-    document.querySelector('.word').classList.remove('btnCorrect');
-    document.querySelector('.word').textContent = '';
+    // document.querySelector('.word').classList.remove('btnWrong')
+    // document.querySelector('.word').classList.remove('btnCorrect');
+    // document.querySelector('.word').textContent = '';
     document.querySelector('.input').value = '';
     document.querySelector('.btn').classList.remove('hide');
     document.querySelector('.Score').textContent = 0;
@@ -50,13 +51,11 @@ catsocialMedia.addEventListener('click', function(){
     catsocialMedia.classList.add('btnDesign');  
     catMedical.classList.remove('btnDesign');
     catSports.classList.remove('btnDesign'); 
+    tilesCon.innerHTML = ''
 })
 catMedical.addEventListener('click', function(){
     mode = true;
     arrWords = arrCat.Medical;
-    document.querySelector('.word').classList.remove('btnWrong')
-    document.querySelector('.word').classList.remove('btnCorrect');
-    document.querySelector('.word').textContent = '';
     document.querySelector('.input').value = '';
     document.querySelector('.btn').classList.remove('hide');
     document.querySelector('.Score').textContent = 0;
@@ -66,14 +65,12 @@ catMedical.addEventListener('click', function(){
     catMedical.classList.add('btnDesign');
     catSports.classList.remove('btnDesign');
     catsocialMedia.classList.remove('btnDesign');  
+    tilesCon.innerHTML = ''
 })
 
 catSports.addEventListener('click', function(){
     mode = true;
     arrWords = arrCat.sport;
-    document.querySelector('.word').classList.remove('btnWrong')
-    document.querySelector('.word').classList.remove('btnCorrect');
-    document.querySelector('.word').textContent = '';
     document.querySelector('.input').value = '';
     document.querySelector('.btn').classList.remove('hide');
     document.querySelector('.Score').textContent = 0;
@@ -83,28 +80,36 @@ catSports.addEventListener('click', function(){
     catSports.classList.add('btnDesign');
     catMedical.classList.remove('btnDesign');
     catsocialMedia.classList.remove('btnDesign');  
-    
+    tilesCon.innerHTML = ''
 })
+
 
 btn.addEventListener('click', function (){
     if (mode === false){
         document.querySelector('.notifier').textContent ='Please Select a Category'
     } else if(mode === true){
+        document.querySelector('.tiles').innerHTML = '';
     const randomize = Math.trunc(Math.random()*arrWords.length);
      arrSingleWrd1 = arrWords[randomize]
         arrSingleWrd = arrSingleWrd1.replace(arrSingleWrd1[0], arrSingleWrd1[0].toUpperCase())
 
     const arrWrdSplit = arrSingleWrd.split('')
     let rearrange = arrWrdSplit.sort(function(a, b) {return 0.5 - Math.random()});
-    let randomized =  rearrange.join('').toLowerCase();
-    document.querySelector('.word').textContent = randomized;
-    document.querySelector('.btn').classList.add('hide');
-    if(document.querySelector('.word').classList.contains('btnWrong')){
-        document.querySelector('.word').classList.remove('btnWrong');
+    console.log(rearrange);
+    rearrange.forEach(mov => {
+        const tiles = document.createElement('div')
+       tiles.classList.add('tiles');
+       tiles.innerHTML = `<div class="item">${mov.toLowerCase()}</div>`;
+       document.querySelector('.tiles').appendChild(tiles);
+       console.log(tiles)
+       const items = Array.from(document.querySelectorAll('.tiles'))
+       console.log(items)
+       items.map(item => item.style.opacity = '100');
     }
-    if(document.querySelector('.word').classList.contains('btnCorrect')){
-        document.querySelector('.word').classList.remove('btnCorrect');
-    }
+        )
+    // forEach()
+    // document.querySelector('.word').textContent = randomized;
+   btn.classList.add('hide');
     document.querySelector('.notifier').textContent ='';
 }
 })
@@ -113,6 +118,7 @@ const rearrangeFunc = function (){
     let inputRaw = document.querySelector('.input').value;
     console.log(inputRaw[0])
     let input =inputRaw.replace(inputRaw[0], inputRaw[0].toLocaleUpperCase())
+  console.log(arrSingleWrd)
     if (input === ''){
         document.querySelector('.notifier').textContent ='Enter a word⛔'
     } else{
@@ -120,10 +126,9 @@ const rearrangeFunc = function (){
           console.log('correct')
           score.textContent++;
           document.querySelector('.input').value = '';
-          document.querySelector('.notifier').textContent ='Correct!✅'
-          document.querySelector('.word').classList.add('btnCorrect')
-          document.querySelector('.word').classList.remove('btnWrong')
-          document.querySelector('.btn').classList.remove('hide');
+          btn.classList.remove('hide')
+          document.querySelector('.notifier').textContent ='✅'
+          document.querySelectorAll('.item').forEach(mov => mov.classList.add('btnCorrect'));
           document.querySelector('.hint').classList.add('hide');
            document.querySelector('.hintBtn').classList.add('hide');
            let noAttempts = document.querySelector('.attemptsV');
@@ -133,17 +138,17 @@ const rearrangeFunc = function (){
              highscore.textContent++;
           }
       } else {
-          console.log('Wrong!')
-          //   document.querySelector('.btn').classList.remove('hide');
-          document.querySelector('.word').classList.add('btnWrong')
+          document.querySelectorAll('.item').forEach( mov => mov.classList.add('btnWrong'))
+          console.log(document.querySelector('.item').textContent)
+        document.querySelector('.item').classList.add('btnWrong');
           let noAttempts = document.querySelector('.attemptsV');
           noAttempts.textContent--;
-          document.querySelector('.notifier').textContent ='Wrong!❌'
-          console.log( noAttempts.textContent)
+          document.querySelector('.notifier').textContent ='❌'
+          console.log(noAttempts.textContent)
           document.querySelector('.tryAgain').classList.remove('hide');
           document.querySelector('.tryAgain').addEventListener('click', function(){
             document.querySelector('.notifier').textContent ='';
-            document.querySelector('.word').classList.remove('btnWrong')
+            document.querySelectorAll('.item').forEach(mov => mov.classList.remove('btnWrong'))
             document.querySelector('.input').value = '';
             document.querySelector('.tryAgain').classList.add('hide');
             if (Number(noAttempts.textContent) === 2){
@@ -167,12 +172,10 @@ const rearrangeFunc = function (){
               document.querySelector('.hint').classList.add('hide');
               score.textContent--;
               noAttempts.textContent = 5;
-              document.querySelector('.input').value = '';
-              document.querySelector('.word').textContent = '';
+              document.querySelector('.input').value = ''
               document.querySelector('.notifier').textContent ='';
               document.querySelector('.tryAgain').classList.add('hide');
               document.querySelector('.btn').classList.remove('hide');
-              document.querySelector('.word').classList.remove('btnWrong')
               if(Number(score.textContent) === 0 || Number(score.textContent) < 0){
                 document.querySelector('.attemptsV').textContent = 5;
                   document.querySelector('.Score').textContent = 0;
@@ -186,7 +189,6 @@ const rearrangeFunc = function (){
 }
 rearrangeBtn.addEventListener('click', rearrangeFunc)
 document.addEventListener('keydown', function(e){
-    console.log(e)
         if(e.key === 'Enter'){
             rearrangeFunc();
         }
@@ -196,9 +198,7 @@ document.addEventListener('keydown', function(e){
 
 reset.classList.remove('hide')
 reset.addEventListener('click', function(){
-    document.querySelector('.word').classList.remove('btnWrong')
-    document.querySelector('.word').classList.remove('btnCorrect');
-    document.querySelector('.word').textContent = '';
+    tilesCon.innerHTML = '';
     document.querySelector('.input').value = '';
     document.querySelector('.btn').classList.remove('hide');
     document.querySelector('.Score').textContent = 0;
